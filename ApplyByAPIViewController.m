5005a57f9223b2411ffc33dbbc669723ba402b0c
ArielSD <ascottdicker@dosomething.org>
2016-06-29 14:11:31 -0400

    Finalize request with NSLogs

--- ApplyByAPIViewController.m
+++ ApplyByAPIViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ApplyByAPIViewController.h"
+#import <AFNetworking.h>
 
 @interface ApplyByAPIViewController ()
 
@@ -16,28 +17,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    NSString *resumeFilePath = @"/Users/Ariel/Desktop/Ariel Scott-Dicker.pdf";
-    NSData *resumeData = [NSData dataWithContentsOfFile:resumeFilePath];
-    
-    NSString *resumeBase64EncodedString = [resumeData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
-    
-    NSLog(@"Base 64 String: %@", resumeBase64EncodedString);
+    [self postResume];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)postResume {
+    AFHTTPSessionManager *sessionManager = [AFHTTPSessionManager manager];
+    sessionManager.requestSerializer = [AFJSONRequestSerializer new];
+    NSString *perkaURLString = @"https://api.perka.com/1/communication/job/apply";
+    
+    NSString *base64EncodedResumeString = [self base64EncodedStringFromFilePath:@"/Users/Ariel/Desktop/Ariel Scott-Dicker.pdf"];
+    NSDictionary *applicationDictionary = @{@"first_name" : @"Ariel",
+                                            @"last_name" : @"Scott-Dicker",
+                                            @"email" : @"ArielSD@gmail.com",
+                                            @"position_ID" : @"IOS",
+                                            @"explanation" : @"Originally, I sent a test through Postman (Yup, that was me). Then, I encoded the PDF using NSData, and made the POST request using AFNetworking",
+                                            @"projects" : @[@"https://github.com/ArielSD", @"Here's the link to my code: ",@"Applying through an API is really cool, by the way"],
+                                            @"source" : @"A friend of mine who is a recruiter",
+                                            @"resume" : base64EncodedResumeString
+                                            };
+    
+    [sessionManager POST:perkaURLString
+              parameters:applicationDictionary
+                progress:nil
+                 success:^(NSURLSessionDataTask *task, id responseObject) {
+                     NSLog(@"Response Object: %@", responseObject);
+                     
+                     NSHTTPURLResponse *response = (NSHTTPURLResponse *)task.response;
+                     NSLog(@"Response Status Code: %lu", response.statusCode);
+                 }
+                 failure:^(NSURLSessionDataTask *task, NSError *error) {
+                     NSLog(@"Error: %@", error.localizedDescription);
+                     
+                     NSHTTPURLResponse *response = (NSHTTPURLResponse *)task.response;
+                     NSLog(@"Response Status Code: %lu", response.statusCode);
+                 }];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSString *)base64EncodedStringFromFilePath:(NSString *)filePath {
+    NSData *data = [NSData dataWithContentsOfFile:filePath];
+    NSString *base64EncodedString = [data base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    return base64EncodedString;
 }
-*/
 
 @end
